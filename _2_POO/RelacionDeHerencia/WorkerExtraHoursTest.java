2d1e1fdf69c2b6ae681efdcaefe0e45b51ae53e7
RinaldiAugusto <augusto.rinaldi.75@gmail.com>
2025-03-26 20:02:31 -0300

    update again..

---
+++ _2_POO/RelacionDeHerencia/WorkerExtraHoursTest.java
@@ -0,0 +1,23 @@
+package _2_POO.RelacionDeHerencia;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class WorkerExtraHoursTest {
+
+    @Tag("unit")
+    @Test
+    void myTest(){
+
+    }
+
+    @Tag("otherTest")
+    @Test
+    void otherTest(){
+
+    }
+
+}
