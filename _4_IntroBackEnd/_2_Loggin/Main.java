9407e4c4cfe759f03c1680e21f24cb6fccf123d3
RinaldiAugusto <augusto.rinaldi.75@gmail.com>
2025-03-27 20:33:45 -0300

    update again..

--- _4_IntroBackEnd/_2_Loggin/Main.java
+++ _4_IntroBackEnd/_2_Loggin/Main.java
@@ -1,16 +1,4 @@
 package _4_IntroBackEnd._2_Loggin;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Main {
-    public static void main(String[] args) throws Exception {
-        AvarageList avarageListGreaterThan5 = null;
-        AvarageList avarageListGreaterThan10 = null;
-        AvarageList avarageListEmpty = null;
-
-        avarageListGreaterThan5 = new AvarageList(Arrays.asList(1, 2, 3, 4, 5, 6));
-        avarageListGreaterThan10 = new AvarageList(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11));
-        avarageListEmpty = new AvarageList(new ArrayList<>());
-    }
 }
