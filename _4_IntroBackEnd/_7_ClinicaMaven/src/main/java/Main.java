9f43bd14f944d8b128be59d55a01f572d516851b
RinaldiAugusto <augusto.rinaldi.75@gmail.com>
2025-04-10 15:05:41 -0300

    update again..

---
+++ _4_IntroBackEnd/_7_ClinicaMaven/src/main/java/Main.java
@@ -0,0 +1,44 @@
+import dao.BD;
+import dao.impl.DentistDaoH2;
+import model.Dentist;
+import service.DentistService;
+
+public class Main {
+    public static void main(String[] args) {
+        DentistService dentistService = new DentistService(new DentistDaoH2());
+
+        //Creamos las tablas
+        BD.createTables();
+
+        //Crear objetos
+        Dentist dentist1 = new Dentist(1234, "Augusto", "Rinaldi");
+        Dentist dentist2 = new Dentist(5678, "Tomas", "Curet");
+        Dentist dentist3 = new Dentist(9101, "Ignacio", "Jauregui");
+        Dentist dentist4 = new Dentist(1213, "Manuel", "Linzoain");
+        Dentist dentist5 = new Dentist(1415, "Agustin", "Alegre");
+
+        dentistService.save(dentist1);
+        dentistService.save(dentist2);
+        dentistService.save(dentist3);
+        dentistService.save(dentist4);
+        dentistService.save(dentist5);
+
+        //Consultar odontologo por id
+        int id = 2;
+        dentistService.findById(id);
+
+        //Actualizar alguno de los atributos de un odontologo
+        String update = "Andres";
+        dentist1.setName(update);
+        dentistService.update(dentist1);
+        System.out.println("El nombre actualizado es: " + dentist1.getName());
+
+        //Borrar alguno de los registros de la tabla
+        int idDelete = 4;
+        dentistService.delete(idDelete);
+
+        //Consultar los registros restantes de la tabla
+        dentistService.findAll();
+
+    }
+}
